312de388e6d30686e6ce32929d171c77717bb307
sgenlecroyant <sgenlecroyant@gmail.com>
2022-06-14 16:07:36 +0200

    created the PUT ENDPOINT

--- src/main/java/com/sgenlecroyant/spring/event/member/api/server/MemberController.java
+++ src/main/java/com/sgenlecroyant/spring/event/member/api/server/MemberController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -38,6 +39,12 @@ public class MemberController {
 		return 
 				ResponseEntity.status(HttpStatus.CREATED)
 							  .body(new MemberResponse(savedMember.getFirstName(), savedMember.getLastName()));
+	}
+	
+	@PutMapping(value = "/api/members")
+	private ResponseEntity<MemberResponse> updateMember(@PathVariable String id, @RequestBody MemberRequest memberRequest){
+		MemberResponse memberResponse = this.memberService.updateMember(id, memberRequest);
+		return new ResponseEntity<MemberResponse>(memberResponse, HttpStatus.OK);
 	}
 
 }
