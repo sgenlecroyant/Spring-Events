7f4917910b5e81490e5e54192f2405ee302acc4b
sgenlecroyant <sgenlecroyant@gmail.com>
2022-06-14 16:01:31 +0200

    created the DELETE ENDPOINT

--- src/main/java/com/sgenlecroyant/spring/event/member/api/server/MemberController.java
+++ src/main/java/com/sgenlecroyant/spring/event/member/api/server/MemberController.java
@@ -3,6 +3,7 @@ package com.sgenlecroyant.spring.event.member.api.server;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +20,12 @@ public class MemberController {
 	public ResponseEntity<MemberResponse> fetchMemberById(@PathVariable String id){
 		MemberResponse memberResponse = this.memberService.fetchMemberById(id);
 		return new ResponseEntity<MemberResponse>(memberResponse, HttpStatus.OK);
+	}
+	
+	@DeleteMapping(value = "/api/members/{id}")
+	private ResponseEntity<Void> deleteMemberById(@PathVariable String id){
+		this.memberService.deleteMemberById(id);
+		return new ResponseEntity<>(HttpStatus.OK);
 	}
 
 }
