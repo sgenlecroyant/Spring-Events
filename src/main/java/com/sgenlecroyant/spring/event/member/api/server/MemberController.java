edc1b2603293fa51a2f0f1af2aa78d57f95ae178
sgenlecroyant <sgenlecroyant@gmail.com>
2022-06-14 15:59:20 +0200

    created the member controller class

---
+++ src/main/java/com/sgenlecroyant/spring/event/member/api/server/MemberController.java
@@ -0,0 +1,24 @@
+package com.sgenlecroyant.spring.event.member.api.server;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sgenlecroyant.spring.event.member.api.response.MemberResponse;
+import com.sgenlecroyant.spring.event.member.service.MemberService;
+
+@RestController
+public class MemberController {
+	@Autowired
+	private MemberService memberService;
+	
+	@GetMapping(value = "/api/members/{id}")
+	public ResponseEntity<MemberResponse> fetchMemberById(@PathVariable String id){
+		MemberResponse memberResponse = this.memberService.fetchMemberById(id);
+		return new ResponseEntity<MemberResponse>(memberResponse, HttpStatus.OK);
+	}
+
+}
