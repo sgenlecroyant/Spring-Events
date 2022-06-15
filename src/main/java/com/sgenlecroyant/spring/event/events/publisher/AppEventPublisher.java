c5ddb8910faf839121990d0837ef2d9dd756e32c
sgenlecroyant <sgenlecroyant@gmail.com>
2022-06-15 20:35:12 +0200

    tested the Events publishing feature

--- src/main/java/com/sgenlecroyant/spring/event/events/publisher/AppEventPublisher.java
+++ src/main/java/com/sgenlecroyant/spring/event/events/publisher/AppEventPublisher.java
@@ -1,5 +1,8 @@
 package com.sgenlecroyant.spring.event.events.publisher;
 
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
@@ -7,9 +10,15 @@ import org.springframework.stereotype.Component;
 @Component
 public class AppEventPublisher {
 	
+	@Autowired
 	private ApplicationEventPublisher applicationEventPublisher;
 	
 	public <T extends ApplicationEvent> void publish(T appEvent){
+		try {
+			TimeUnit.SECONDS.sleep(20);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 		this.applicationEventPublisher.publishEvent(appEvent);
 	}
 
