d5f303e7b2bcf4c347f9200db6ab9f22b8675088
sgenlecroyant <sgenlecroyant@gmail.com>
2022-06-15 20:43:48 +0200

    configured the Event to be multithreaded to avoid blocking requests and become asynchronous

---
+++ src/main/java/com/sgenlecroyant/spring/event/events/EventConfig.java
@@ -0,0 +1,28 @@
+package com.sgenlecroyant.spring.event.events;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.ApplicationEventMulticaster;
+import org.springframework.context.event.SimpleApplicationEventMulticaster;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+
+@Configuration
+public class EventConfig {
+	
+	@Bean(name = AbstractApplicationContext.APPLICATION_EVENT_MULTICASTER_BEAN_NAME)
+	public ApplicationEventMulticaster getApplicationEventMulticaster() {
+		
+		SimpleApplicationEventMulticaster applicationEventMulticaster = 
+				new SimpleApplicationEventMulticaster();
+		
+		SimpleAsyncTaskExecutor taskExecutor = new SimpleAsyncTaskExecutor("Event-Publishing-Thread");
+		taskExecutor.setConcurrencyLimit(-1);
+		taskExecutor.setThreadPriority(5);
+		applicationEventMulticaster.setTaskExecutor(taskExecutor);
+		
+		return applicationEventMulticaster;
+		
+	}
+
+}
